0a800b70cf9e0bd1a39cdba31b02c2c12373b461
Faruk Becirovic <farukbecc@gmail.com>
2022-04-14 21:40:52 +0200

    updated module

--- src/NumericalMethods.jl
+++ src/NumericalMethods.jl
@@ -1,5 +1,15 @@
 module NumericalMethods
 
-# Write your package code here.
+include("bisection_method.jl")
+include("divided_difference.jl")
+include("hermite_interpolation.jl")
+include("newtons_method.jl")
+include("secant_method.jl")
+
+export bisect
+export newton
+export secant
+export divid
+export hermit
 
 end
